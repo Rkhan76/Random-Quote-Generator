3857022a6dcbd70c416043bc2cefb3f0f5b3fbde
Rakhshan khan <rakshaankhan1@gmail.com>
2024-01-29 14:46:18 +0530

    Transfer javascript code to script file

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let quotesData
+const url = 'https://type.fit/api/quotes'
+fetch(url)
+  .then((response) => {
+    console.log('hllo')
+    return response.json()
+  })
+  .then((data) => {
+    quotesData = data
+  })
 
+function generateQuote() {
+  let randomNum = Math.floor(Math.random() * 15 + 1)
+  let quote = quotesData[randomNum]
+  let authorName = quote.author.split(',')[0]
+  document.getElementById('quote').innerText = quote.text
+  document.getElementById('author').innerText = authorName
+}
